8188ce2fda0e84e6ec78da0d6db9827a85afa596
Jabbar Azhar Mujeeb <67254298+azharmujeeb@users.noreply.github.com>
2021-06-03 13:44:24 +0530

    Sign Up
    connected to the database successfully.

--- Admin Website/signup.js
+++ Admin Website/signup.js
@@ -1,4 +1,4 @@
-const jsEmail = document.getElementById("email");
+/*const jsEmail = document.getElementById("email");
 const jsPass = document.getElementById("password");
 
 const jsSignupBtn = document.getElementById("signupbtn");
@@ -17,4 +17,61 @@ jsSignupBtn.addEventListener('click', (e) => {
 
 	});
 
-});
+});*/
+
+
+
+
+
+
+/*284 code*/
+  var firebaseConfig = {
+    apiKey: "AIzaSyDB-6U09zokZcEa0ccnyqfqaGMn8WI1_XQ",
+    authDomain: "main-7ab9c.firebaseapp.com",
+    databaseURL: "https://main-7ab9c-default-rtdb.firebaseio.com",
+    projectId: "main-7ab9c",
+    storageBucket: "main-7ab9c.appspot.com",
+    messagingSenderId: "108058188999",
+    appId: "1:108058188999:web:af2dd087a15554733732e2",
+    measurementId: "G-ZL6XBQNDZ8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //referencing the user
+  let signupinfo = firebase.database().ref("Web Sign Up");
+
+  //Listen for a submit
+  document.querySelector(".signup-form").addEventListener("submit", submitForm);
+
+
+
+  function submitForm(e){
+
+  	e.preventDefault();
+
+  	//Get input values
+  	let email = document.querySelector("#email").value;
+  	let password = document.querySelector("#password").value;
+
+
+  	console.log(email, password);
+  	saveSignupinfo(email, password);
+  	document.querySelector(".signup-form").reset();
+
+  }
+
+
+  //Save Info to firebase database
+  function saveSignupinfo(email, password){
+
+  	let newSignupInfo = signupinfo.push(); 
+
+  	newSignupInfo.set({
+  		email: email,
+  		password: password,
+  	});
+
+  	window.alert("Details saved.");
+
+  };
