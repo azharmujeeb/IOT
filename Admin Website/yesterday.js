aa5e8433916cf17de8b277dbced036c7a0df85df
Jabbar Azhar Mujeeb <67254298+azharmujeeb@users.noreply.github.com>
2021-06-06 14:10:14 +0530

    yesterday updatting

---
+++ Admin Website/yesterday.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDB-6U09zokZcEa0ccnyqfqaGMn8WI1_XQ",
+    authDomain: "main-7ab9c.firebaseapp.com",
+    databaseURL: "https://main-7ab9c-default-rtdb.firebaseio.com",
+    projectId: "main-7ab9c",
+    storageBucket: "main-7ab9c.appspot.com",
+    messagingSenderId: "108058188999",
+    appId: "1:108058188999:web:af2dd087a15554733732e2",
+    measurementId: "G-ZL6XBQNDZ8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var YOTimeRef = firebase.database().ref('WebYesterdayOpeningTime');
